8911635caca3ed81851c0a1029c4bf5f676f6440
Joe tay <tay.joe@gmail.com>
2024-02-16 08:17:10 +0800

    ssl

--- main.py
+++ main.py
@@ -72,12 +72,12 @@ import configparser
 import ast
 import ssl
               
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 
 
